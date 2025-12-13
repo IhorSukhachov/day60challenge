5ddf96df4d9eaae8c5d165860ff613681434945b
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-13 13:32:42 +0200

    Initial Commit

---
+++ day60challenge/day60challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  day60challengeApp.swift
+//  day60challenge
+//
+//  Created by Ihor Sukhachov on 13.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct day60challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
