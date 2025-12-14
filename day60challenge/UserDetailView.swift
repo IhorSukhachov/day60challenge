64c47747085415736598b75e2651d18163185060
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-14 09:51:43 +0200

    creating user detailed view file

---
+++ day60challenge/UserDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  UserDetailView.swift
+//  day60challenge
+//
+//  Created by Ihor Sukhachov on 14.12.2025.
+//
+
+import SwiftUI
+
+struct UserDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDetailView()
+}
